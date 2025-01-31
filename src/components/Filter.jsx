19f2774c1f9d98d5cca0f3bb14edc92bfdb502c1
Mert Özbolat <mertozbolat008@gmail.com>
2025-01-31 15:58:29 +0300

    update

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -13,17 +13,17 @@ const Filter = () => {
 
 
     return (
-        <div className='container'>
-            {
-                categories.map((item) => {
-                    return (
-                        <div key={item.id}>
-                            <h1>{item.name}</h1>
-                        </div>
-                    )
-                })
-            }
+        <div className="container mt-5">
+            <div className="d-flex flex-wrap justify-content-between">
+                {categories.map((item) => (
+                    <div key={item.id} className="d-flex flex-column align-items-center rounded m-1 flex-grow-1">
+                        <img src={item.images} alt={item.name} className="mb-2" style={{ width: "30px", height: "30px" }} />
+                        <span className=" fw-bold text-dark text-center">{item.name}</span>
+                    </div>
+                ))}
+            </div>
         </div>
+
     )
 }
 
